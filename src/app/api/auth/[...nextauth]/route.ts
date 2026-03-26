51c88e447ac40e6409c70b9099158fded3382929
jkwx1900 <hyacinthewynell@gmail.com>
2026-03-26 16:40:44 +0800

    use nextauth v4 API

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,38 @@
-import { handlers } from "@/lib/auth";
+import NextAuth from "next-auth";
 
-export const { GET, POST } = handlers;
+export default NextAuth({
+  providers: [
+    {
+      id: "google",
+      name: "Google",
+      type: "oauth",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        url: "https://accounts.google.com/o/oauth2/v2/auth",
+        params: { scope: "openid email profile" }
+      },
+      token: "https://oauth2.googleapis.com/token",
+      userinfo: "https://www.googleapis.com/oauth2/v3/userinfo",
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    },
+  ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session.user) {
+        session.user.id = token.sub!;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+});
